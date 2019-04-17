1dad4601f67a34fb5815662f1064dcc5c4803e84
nnikka <nikakhurashvili@gmail.com>
2019-04-17 18:26:42 +0400

    delete imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component, Input } from '@angular/core';
 import InputBase from './models/InputBase'
 
-import TextInput from './models/TextInput'
-import NumberInput from './models/NumberInput'
-import CheckboxInput from './models/CheckboxInput'
-import DateInput from './models/DateInput'
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
